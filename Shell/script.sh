dce1f62a6793ccd083717875a3cabcafd9b10dac
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2023-04-15 10:57:26 +0300

    feat(shell): add initial config

---
+++ Shell/script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# Alpine Linux only has sh and ash installed by default. If you need to use a
+# different shell, you'll need to install it via apk add --no-cache bash
+
+echo "Hello World!"
